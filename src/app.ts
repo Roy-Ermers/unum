be7df25c8b5a6ad0b4cded5b14944e3b61765687
Roy Ermers <roy@roy-daan.nl>
2020-04-02 17:13:57 +0200

    reordered routes

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,11 @@ import { RoomManager } from "./classes/RoomManager";
 import logger from "./logger";
 const App = express();
 
+App.get("/logs", (req, res) => res.send(logger.log));
 App.use(express.static("static", { extensions: ["html", "htm"] }));
 App.use("*", (req, res) => {
 	res.status(404).send("Not found");
 });
-App.get("/logs", (req, res) => res.send(logger.log));
 
 console.log(`Uno listening on ${process.env.PORT ?? 8080}`);
 const IO = SocketIO(App.listen(process.env.PORT ?? 8080));
